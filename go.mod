16a62997245c5645ef064997d6d6614adddb2043
Klinton Lee <klintonlee92@gmail.com>
2021-10-21 18:29:31 -0300

    Working with JSON and handling responses gracefully

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/gorilla/mux v1.8.0
 	github.com/jinzhu/gorm v1.9.16
 	github.com/joho/godotenv v1.4.0
+	github.com/satori/go.uuid v1.2.0
 )
 
 require (
--- internal/transport/http/handler.go
+++ internal/transport/http/handler.go
@@ -1,18 +1,26 @@
 package http
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/KlintonLee/go-rest-api-course/internal/database/models"
 	"github.com/KlintonLee/go-rest-api-course/internal/database/repositories"
 	"github.com/gorilla/mux"
+	uuid "github.com/satori/go.uuid"
 )
 
 // Handler - stores the pointer to our comments service
 type Handler struct {
 	Router               *mux.Router
 	CommentsRepositoryDb *repositories.CommentsRepositoryDB
+}
+
+type Response struct {
+	Message string
+	Error   string
 }
 
 // NewHandler - returns a pointer to a Handler
@@ -34,7 +42,12 @@ func (h *Handler) SetupRoutes() {
 	h.Router.HandleFunc("/api/comments/{id}", h.DeleteComment).Methods("DELETE")
 
 	h.Router.HandleFunc("/api/health", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "I am alive!")
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		w.WriteHeader(http.StatusOK)
+
+		if err := json.NewEncoder(w).Encode(Response{Message: "I am Alive"}); err != nil {
+			panic(err)
+		}
 	})
 }
 
@@ -45,51 +58,96 @@ func (h *Handler) GetComment(w http.ResponseWriter, r *http.Request) {
 
 	comment, err := h.CommentsRepositoryDb.GetComment(id)
 	if err != nil {
-		fmt.Fprintf(w, "Error retrieving Comment by ID")
+		sendErrorResponse(w, "Error retrieving Comment by ID", err)
+		return
 	}
 
-	fmt.Fprintf(w, "%+v", comment)
+	if sendOkResponse(w, comment, http.StatusOK); err != nil {
+		panic(err)
+	}
 }
 
+// GetAllComments - retrieve all created comments
 func (h *Handler) GetAllComments(w http.ResponseWriter, r *http.Request) {
 	comments, err := h.CommentsRepositoryDb.GetAllComments()
 	if err != nil {
-		fmt.Fprintf(w, "Error retrieving comments")
+		sendErrorResponse(w, "Error retrieving comments", err)
+		return
 	}
 
-	fmt.Fprintf(w, "%+v", comments)
+	if err := sendOkResponse(w, comments, http.StatusOK); err != nil {
+		panic(err)
+	}
 }
 
+// PostComment - create a new comment
 func (h *Handler) PostComment(w http.ResponseWriter, r *http.Request) {
-	comment, err := h.CommentsRepositoryDb.PostComment(&models.Comment{
-		ID:   "154cf9a4-f226-4fc6-a7f0-c244811d6064",
-		Slug: "/",
-	})
-	if err != nil {
-		fmt.Fprintf(w, "Failed to post new comment")
+	var comment models.Comment
+	comment.ID = uuid.NewV4().String()
+	comment.CreatedAt = time.Now()
+
+	if err := json.NewDecoder(r.Body).Decode(&comment); err != nil {
+		sendErrorResponse(w, "Failed to decode JSON Body", err)
+		return
 	}
 
-	fmt.Fprintf(w, "%+v", comment)
+	_, err := h.CommentsRepositoryDb.PostComment(&comment)
+	if err != nil {
+		sendErrorResponse(w, "Failed to post new comment", err)
+		return
+	}
+
+	if err := sendOkResponse(w, comment, http.StatusCreated); err != nil {
+		panic(err)
+	}
 }
 
+// UpdateComment - updates an existing comment by id
 func (h *Handler) UpdateComment(w http.ResponseWriter, r *http.Request) {
-	comment, err := h.CommentsRepositoryDb.UpdateComment("154cf9a4-f226-4fc6-a7f0-c244811d6064", &models.Comment{
-		Slug: "/updated",
-	})
-	if err != nil {
-		fmt.Fprintf(w, "Failed to update the comment")
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	var comment models.Comment
+	if err := json.NewDecoder(r.Body).Decode(&comment); err != nil {
+		sendErrorResponse(w, "Failed to decode JSON Body", err)
+		return
 	}
 
-	fmt.Fprintf(w, "%+v", comment)
+	updatedComment, err := h.CommentsRepositoryDb.UpdateComment(id, &comment)
+	if err != nil {
+		sendErrorResponse(w, "Failed to update the comment", err)
+		return
+	}
+
+	if err := sendOkResponse(w, updatedComment, http.StatusOK); err != nil {
+		panic(err)
+	}
 }
 
+// DeleteComment - delete an existing comment by id
 func (h *Handler) DeleteComment(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(http.StatusNoContent)
+
 	vars := mux.Vars(r)
 	id := vars["id"]
 
 	if err := h.CommentsRepositoryDb.DeleteComment(id); err != nil {
-		fmt.Fprintf(w, "Failed to delete the comment")
+		sendErrorResponse(w, "Failed to delete the comment", err)
+		return
 	}
+}
 
-	fmt.Fprintf(w, "Successfully deleted comment")
+func sendOkResponse(w http.ResponseWriter, resp interface{}, statusCode int) error {
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(statusCode)
+	return json.NewEncoder(w).Encode(resp)
+}
+
+func sendErrorResponse(w http.ResponseWriter, message string, err error) {
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(http.StatusInternalServerError)
+	if err := json.NewEncoder(w).Encode(Response{Message: message, Error: err.Error()}); err != nil {
+		panic(err)
+	}
 }
